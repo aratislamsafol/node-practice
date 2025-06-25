b3ab595e2da79d8c1d02878b27b94749ce9ed78c
aratislamsafol <aratislamsafol@gmail.com>
2025-06-26 01:26:15 +0600

    delete

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ var http = require('http');
 var server = http.createServer(function ( req, res ) {
     if(req.url == "/") {
         // file write Async
-       fs.rename('demo.txt', 'new-demo.txt', function(err){
+       fs.unlink( 'new-demo.txt', function(err){
         if(err) {
             res.writeHead(200, {'Content-type': 'text/html'});
-            res.write("Failed to Write");
+            res.write("Failed to Delete");
             res.end();
         }else{
             res.writeHead(200, {'Content-type': 'text/html'});
-            res.write("Write Successfully ");
+            res.write("Delete Successfully ");
             res.end();
         }
        })
     } else if(req.url == '/about') {
         // sync write file
          try {
-            fs.renameSync('sync-demo.txt', 'sync-new.txt');
+            fs.unlinkSync('sync-new.txt');
             res.writeHead(200, { 'Content-type': 'text/html' });
-            res.write("Write Successfully");
+            res.write("Delete Successfully");
             res.end();
         } catch (err) {
             res.writeHead(200, { 'Content-type': 'text/html' });
-            res.write("Failed to rename");
+            res.write("Failed to Delete");
             res.end();
         }
     } else {
